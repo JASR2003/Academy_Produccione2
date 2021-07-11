1a5e422c5ec8d305ae922b1df9fc87ede500a658
Jorge Suárez <suarezjorge758@gmail.com>
2021-07-11 11:23:21 -0400

    1152 px listo

---
+++ scripts.js
@@ -0,0 +1,27 @@
+jQuery("document").ready(function($){
+
+    var menuBtn = $(".menu-icon"),
+        menu = $(".navigation ul");
+
+    menuBtn.click(function() {
+
+        if( menu.hasClass("show")){
+
+            menu.removeClass("show");
+        } else{
+
+            menu.addClass("show");
+        }
+    });
+    menu.click(function() {
+
+        if( menu.hasClass("show")){
+
+            menu.removeClass("show");
+        } else{
+
+            menu.addClass("show");
+        }
+    });
+
+});
